065f6fc98231dbaabe0d819683f10d4611117d21
Kennedy Gitau <kennedygitaukg12@gmail.com>
2026-07-10 02:19:24 +0300

    feat: add backend foundation modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { BranchesModule } from './branches/branches.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, BranchesModule],
   controllers: [AppController],
   providers: [AppService],
 })
