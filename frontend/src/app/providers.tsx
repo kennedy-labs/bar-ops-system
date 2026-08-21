db9b6ee7320593330a0046f069b29d26153984ce
Kennedy Gitau <kennedygitaukg12@gmail.com>
2026-08-21 12:54:24 +0300

    Update providers configuration

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,10 +1,16 @@
-'use client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import "./globals.css";
+import Providers from "./providers";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
   );
 }
